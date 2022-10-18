b0e1320b3a2f996bfed2091feb40fe9159cde6be
minguins <shinminjin@kakao.com>
2022-10-18 15:31:34 +0900

    Initialize #6 - bootstrap ui
    bootstrap 부분 다시 설정 진행함

--- resources/lang/ko/auth.php
+++ resources/lang/ko/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => '일치하지 않습니다',
-    'user_pw' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '일치하는 정보가 없습니다.',
+    'password' => '비밀번호를 확인해 주세요.',
+    'throttle' => '로그인 시도가 너무 많습니다. :seconds 초 후에 다시 시도해 주세요.',
 
 ];
