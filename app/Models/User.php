4b84ac369ea3fb28008cdf2778cf485f789746eb
minguins <shinminjin@kakao.com>
2022-10-18 10:13:54 +0900

    Update #4 - User Model modify
    사용자 정보에 맞게 사용자 이름, 별명, 아이디, 패스워드 추가함

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'user_name',
+        'user_nickname',
+        'user_pw',
+        'user_id',
     ];
 
     /**
@@ -29,8 +30,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'user_pw',
+        'user_id',
     ];
 
     /**
@@ -39,6 +40,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        
     ];
 }
