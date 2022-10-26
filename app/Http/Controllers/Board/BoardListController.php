40132c03b23823e84a9600a927c5d246664dad77
minguins <shinminjin@kakao.com>
2022-10-26 09:13:17 +0900

    Add #12 - Get board contents and render list view
    게시글 리스트를 가지고 와서 뷰 화면에 출력할 수 있도록 데이터 전달함

--- app/Http/Controllers/Board/BoardListController.php
+++ app/Http/Controllers/Board/BoardListController.php
@@ -3,18 +3,45 @@
 namespace App\Http\Controllers\Board;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class BoardListController extends Controller
 {
     /**
-     * Show the application dashboard.
+     * Show the board list
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('boards/list');
+    public function index(Request $request)
+    {   
+        $pageNum = isset($request->all()->pagenum) ? $request->all()->pagenum : 5;
+        $data = $this->getBoardList($pageNum);
+
+        return view('boards/list', [
+            'boards' => $data['boards'],
+            'paginate' => $data['limit_pageNum']
+        ]);
+
     }
+
+    /**
+     * Get the list of boards content
+     *
+     * @return array 
+     */
+
+    public function getBoardList($limit_pageNum){
+        $query = DB::table('board')
+                    ->leftJoin('users', 'users.id', '=', 'board.user_id')
+                    ->where('board.board_flag', 'Y')
+                    ->orderBy('created_at','desc');
+
+        $boards = $query->addSelect('users.user_nickname as user_nickname','board.id as id', 'board.board_title as board_title', 'board.created_at as created_at', 'board.board_hit as board_hit')->paginate($limit_pageNum);
+        return array('limit_pageNum'=> $limit_pageNum, 'boards' => $boards);
+    }
+
+
+    
     
 }
